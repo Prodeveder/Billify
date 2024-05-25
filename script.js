bece721a7770b11da7a4f10d81652c5595aaa8e6
Prodeveder <72999777+Prodeveder@users.noreply.github.com>
2024-05-25 16:21:39 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("JavaScript is ready to run");
+const option = document.querySelectorAll(".option");
+const businessReg = document.querySelector('.businessReg')
+
+option.forEach((Element) => {
+  Element.addEventListener("click", () => {
+    option.forEach((Element) => { 
+        Element.classList.remove("active");
+      });
+
+    Element.classList.add("active");
+
+    if (Element.classList[1] == 'yes') {
+        businessReg.style.display = 'block'
+    } else if (Element.classList[1] == 'no') {
+        businessReg.style.display = 'none';
+
+    }
+  });
+});
